5421d6d8403bcb47b595f7c6784314c34d8b9a4c
HHHUUUGGGOOO <hugo551565@gmail.com>
2021-05-03 16:10:39 +0800

    plot

---
+++ Case_2/src/main.cpp
@@ -0,0 +1,103 @@
+/*****************************************************************************************
+  FileName     [ main.cpp ]
+  PackageName  [ Auto Vehicle in Roundabout ]
+  Synopsis     [ Main function ]
+  Author       [ Yen-Yu, Chen & Hugo, Chen ]
+  Professor    [ Iris Jiang & Chung-Wei, Lin ]
+  Copyright    [ Copyleft(c) 2021, NTUEE, Taiwan ]
+*****************************************************************************************/
+//----------------------------------------------------------------------------------------
+//    Include 
+//----------------------------------------------------------------------------------------
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include "ra_mgr.h"
+
+using namespace std;
+
+//----------------------------------------------------------------------------------------
+//    Global Manager
+//----------------------------------------------------------------------------------------
+ra_mgr* raMgr = new ra_mgr();
+
+//----------------------------------------------------------------------------------------
+//    Main Function
+//----------------------------------------------------------------------------------------
+
+int main(int argc, char** argv)
+{
+  ////// read input file //////
+  // argv[1]:input file, argv[2]:input_ra_file, argv[3]:output_file
+  if (argc == 4) 
+  {
+    if (!raMgr->read_ra_info(argv[2])) {
+      cerr << "Error: cannot open roundabout information input file \"" << argv[2] << "\"!!\n";
+      return 0;
+    }
+    if (!raMgr->read_vehicle(argv[1])) {
+      cerr << "Error: cannot open vehicle information input file \"" << argv[1] << "\"!!\n";
+      return 0;
+    }
+  }
+  else 
+  {
+    cerr << "Error: illegal number of argument (" << argc << ")!!\n";
+    return 0;
+  }
+  
+  string in;
+  while (1)
+  {
+    cerr << "Show roundabout information? (y/n)" << endl;
+    cin >> in;
+    if (in == "y")
+    {
+      raMgr->Roundabout_information();
+      break;
+    }
+    else if (in == "n")
+        break;
+    else
+      cerr << "unknown answer OAO!!" << endl;
+  }
+
+  while (1)
+  {
+    cerr << "Show vehicle information? (y/n)" << endl;
+    cin >> in;
+    if (in == "y")
+    {
+      raMgr->Vehicle_information();
+      break;
+    }
+    else if (in == "n")
+        break;
+    else
+      cerr << "unknown answer OAO!!" << endl;
+  }
+
+
+  ////// schedule //////
+  while (1)
+  {
+    cerr << "Run schedule? (y/n)" << endl;
+    cin >> in;
+    if (in == "y")
+    {
+      raMgr->line_trivial_solution_case_2();
+      break;
+    }
+    else if (in == "n")
+        break;
+    else
+      cerr << "unknown answer OAO!!" << endl;
+  }
+
+  ////// write output file //////
+  raMgr->output_solution(argv[3]);
+
+  delete raMgr;
+  
+  return 0;
+}
--- Case_2/src/ra_mgr.cpp
+++ Case_2/src/ra_mgr.cpp
@@ -136,61 +136,11 @@ ra_mgr::read_ra_info(const string& rafile)
 }
 
 void 
-ra_mgr::line_trivial_solution_case_1()
+ra_mgr::line_trivial_solution_case_2()
 {
     if (!v_total.size()) { cerr << "There is no vehicles to schedule !!" << endl; return; }
     int n_vehicle = wait_list.size();
-  // in ra time
-    for (int i = 0 ; i < n_vehicle ; i++)
-    {
-      float run_time = ra_radius*degree_to_rad(wait_list[i]->destination_angle-wait_list[i]->source_angle)/wait_list[i]->velocity;
-      // 無條件進入到小數點後第一位
-      run_time = ceil(run_time*10 + 0.5)/10;
-      in_ra_time.push_back(run_time);
-    }
-    // The first vehicle
-    real_enter_time.push_back(wait_list[0]->earliest_arrival_time);
-    // Do while traversing all vehicles in the wait_list
-    for (int i = 1 ; i < n_vehicle ; i++)
-    {
-      float can_enter_time = real_enter_time[i-1] + in_ra_time[i];
-      real_enter_time.push_back(max(wait_list[i]->earliest_arrival_time, can_enter_time));
-    }
-    // debug 
-    cout << "====================" << endl;
-    cout << "Vehicle in ra time : " << endl;
-    cout << "====================" << endl;
-    for (int i = 0 ; i < in_ra_time.size() ; i++)
-    {
-      cerr << wait_list[i]->id << " -> " << in_ra_time[i] << " (s)" << endl;
-    }
-    cout << endl;
-    cout << "=========================" << endl;
-    cout << "Vehicle real enter time : " << endl;
-    cout << "=========================" << endl;
-    for (int i = 0 ; i < real_enter_time.size() ; i++)
-    {
-      cerr << wait_list[i]->id << " -> " << real_enter_time[i] << " (s)" << endl;
-    }
-  // make pair of position
-    for (int i = 0 ; i < n_vehicle ; i++)
-    {
-      for (int j = 0 ; j < (int)in_ra_time[i]/0.1 ; j++)
-      {
-        if (j == (int)in_ra_time[i]/0.1 - 1)
-        {
-          float t = real_enter_time[i] + (ra_radius*degree_to_rad(wait_list[i]->destination_angle-wait_list[i]->source_angle)/wait_list[i]->velocity);
-          float angle = wait_list[i]->destination_angle;
-          wait_list[i]->position.push_back(make_pair(t, angle));
-        }
-        else
-        {
-          float t = real_enter_time[i] + 0.1*j;
-          float angle = wait_list[i]->source_angle + rad_to_degree(wait_list[i]->velocity*0.1*j/ra_radius);
-          wait_list[i]->position.push_back(make_pair(t, angle));
-        }
-      }
-    }
+  
 }
 
 
